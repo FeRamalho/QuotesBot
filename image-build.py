181f7e9cb324e2063a3e4bab584c1656efd202e7
FeRamalho <olliveira.fr@gmail.com>
2021-01-06 00:31:19 -0300

    cleaning code and adding more templates images

--- image-build.py
+++ image-build.py
@@ -1,13 +1,43 @@
 from PIL import Image, ImageFont, ImageDraw 
+import textwrap
 
-my_image = Image.open("images/quote-supergirl.png")
+def main():
+    my_image = Image.open("images/quote-supergirl04.png")
 
-title_text = "\"Teste\""
+    test = "\"aqui está um texto com vários caracteres, queria dizer que isso é muito estranho, imagino que não seja tanto assim, thunder, ele repete muito essa palavra, não gosto de músicas que\""
+    test = "\"I love woooo\""
+    #test = "\"iraaaa isso é muuuito estranho, imagino que não\""
 
-title_font = ImageFont.truetype('fonts/Roboto-Regular.ttf', 50)
+    margin = 611
+    offset = 14
 
-image_editable = ImageDraw.Draw(my_image)
+    if len(test) <= 100:
+        offset = 212
 
-image_editable.text((660,124), title_text, (245, 232, 230), font=title_font)
+    title_font = ImageFont.truetype('fonts/Roboto-Regular.ttf', 50)
+    italic_font = ImageFont.truetype('fonts/Roboto-Italic.ttf', 50)
 
-my_image.save("result.png")
+    image_editable = ImageDraw.Draw(my_image)
+
+    text = "\n".join(textwrap.wrap(test, width=23))
+
+    image_editable.text((margin, offset), text, (241, 19, 38), font=title_font)
+
+    offset += (title_font.getsize(text)[1] * len(textwrap.wrap(test, width=23)))
+
+    character = "-Supergirl"
+    image_editable.text((margin, offset), character, (178, 30, 42), font=italic_font)
+
+    my_image.save("result.png")
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+#250 caracteres
+#183 caracteres sem precisar mudar a fonte
+#212 -> -100caracteres
+#cortar a frase se tiver mais de 183 chars
+#cor -> (245, 232, 230)
